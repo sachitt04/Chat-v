b2495e09179ba05a307796d784f9cc0a45a015c0
sachit <sachitforsocials@gmail.com>
2024-09-14 11:23:05 +0530

    create header and update applayout

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,8 +1,76 @@
 import React from 'react'
-
+import { AppBar, Box ,IconButton,Toolbar, Typography } from '@mui/material'
+import { orange ,} from '../../constants/color'
+import {Menu as MenuIcon, Search as SearchICon,Add as AddIcon} from '@mui/icons-material';
+ 
 const Header = () => {
+  // handleMobile function
+  const handleMobile = () => {
+        console.log("handleMoblie")
+  }  
+
+  // open search dialog function
+  const openSearchDialog = () => {
+    console.log("openSearchDialog")
+  }
+
+  // open new group function
+  const openNewGroup = () => {
+    console.log("openNewGroup")
+  }
+
   return (
-    <div>Header</div>
+     <>
+     <Box
+     sx={{
+      flexGrow:1,
+     }}
+     height={"4rem"}
+
+     >
+      <AppBar position='static' sx={{bgcolor: orange}}>
+        <Toolbar>
+        <Typography variant='h6'
+ 
+        sx={{
+          display:{
+            xs:"none",sm:"block",
+            
+          }
+        }}
+          >Chat-v
+          </Typography>
+          <Box
+          sx={{
+            display:{
+              xs:"block",sm:"none"}}}>
+                
+                {/* Menu button */}
+            <IconButton color='inherit' onClick={handleMobile}>
+              <MenuIcon /> 
+            </IconButton>
+          </Box>
+
+          <Box sx={{flexGrow:1}} /> 
+
+          <Box>
+            {/* Search button */}
+          <IconButton color='inherit' size='large' onClick={openSearchDialog}>           
+            <SearchICon/> 
+            </IconButton>
+
+            {/* Add button */}
+            <IconButton color='inherit' size='large' onClick={openNewGroup}>
+              <AddIcon/>
+            </IconButton>
+          </Box>
+
+        </Toolbar>
+
+        </AppBar>
+
+     </Box>
+     </>
   )
 }
 
---
+++ client/src/constants/color.js
@@ -0,0 +1,2 @@
+export const orange = "#ea7070"
+export const height = "calc(100vh - 4rem)"
